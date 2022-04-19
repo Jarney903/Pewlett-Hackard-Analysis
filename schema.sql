6b8764e2dfc4fc07533f63b500b719c4ebb8ba83
John Arney <Jarney903@gmail.com>
2022-04-19 16:16:56 -0500

    Completed Module 7.2

--- schema.sql
+++ schema.sql
@@ -18,7 +18,7 @@ CREATE TABLE employees (
 	 
 -- Dep_Manager 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -37,4 +37,29 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
-SELECT * FROM departments;
+-- Dep_EMP
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+-- Titles 
+CREATE TABLE titles (
+    emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no, title, from_date)
+);
+
+
+
+SELECT * FROM titles;
+
+--DROP TABLE dept_manager CASCADE;
